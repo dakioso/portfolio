dd3ccb9cabc50d4cdac171fcd49bdfcb374f5e2f
dakioso <daniel.milojevic@student.kyh.se>
2017-12-12 10:35:04 +0100

    Scroll speed

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1600);
+    }, 1200);
 });
 
 $('#send-message').click(function(e){
